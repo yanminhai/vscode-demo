b311d0b8f4e273bf2d5830484c475453ad13eea7
yanminhai <736716935@qq.com>
2025-04-25 16:10:34 +0800

    注释脚本

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,50 @@
+
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+require('./build/gulpfile');
 
-import gulp from 'gulp';
-import path from 'path';
-import fs from 'fs';
-import cp from 'child_process';
-import util from './build/lib/util.js';
-import task from './build/lib/task.js';
-import { buildBackendTask, packageBackendTask } from './build/gulpfile.backend.js';
 
-// Load all the gulpfiles
-import './build/gulpfile.vscode.js';
-import './build/gulpfile.vscode.win32.js';
-import './build/gulpfile.vscode.linux.js';
-import './build/gulpfile.hygiene.js';
-import './build/gulpfile.extensions.js';
-import './build/gulpfile.compile.js';
+// import gulp from 'gulp';
+// import task from './build/lib/task.js';
+// import { buildBackendTask, packageBackendTask } from './build/gulpfile.backend.js';
 
-// Modify the vscode tasks to include backend build
-const platforms = ['win32', 'linux'];
-const architectures = ['x64', 'arm64'];
+// // Load all the gulpfiles
+// import './build/gulpfile.vscode.js';
+// import './build/gulpfile.vscode.win32.js';
+// import './build/gulpfile.vscode.linux.js';
+// import './build/gulpfile.hygiene.js';
+// import './build/gulpfile.extensions.js';
+// import './build/gulpfile.compile.js';
 
-platforms.forEach(platform => {
-	architectures.forEach(arch => {
-		const vscodeTask = gulp.task(`vscode-${platform}-${arch}`);
-		if (vscodeTask) {
-			const backendTask = task.define(`backend-${platform}-${arch}`, task.series(
-				buildBackendTask(platform, arch),
-				packageBackendTask(platform, arch)
-			));
-			gulp.task(backendTask);
+// // Modify the vscode tasks to include backend build
+// const platforms = ['win32', 'linux'];
+// const architectures = ['x64', 'arm64'];
 
-			// Add backend build as a dependency to vscode build
-			gulp.task(`vscode-${platform}-${arch}`, task.series(
-				backendTask,
-				vscodeTask
-			));
-		}
-	});
-});
+// platforms.forEach(platform => {
+// 	architectures.forEach(arch => {
+// 		const vscodeTask = gulp.task(`vscode-${platform}-${arch}`);
+// 		if (vscodeTask) {
+// 			const backendTask = task.define(`backend-${platform}-${arch}`, task.series(
+// 				buildBackendTask(platform, arch),
+// 				packageBackendTask(platform, arch)
+// 			));
+// 			gulp.task(backendTask);
+
+// 			// Add backend build as a dependency to vscode build
+// 			gulp.task(`vscode-${platform}-${arch}`, task.series(
+// 				backendTask,
+// 				vscodeTask
+// 			));
+// 		}
+// 		gulp.task('watch-client', function () {
+// 			// 定义监控客户端文件的逻辑
+// 			return gulp.watch(['path/to/client/files/**/*.ts'], gulp.series('build-client'));
+// 		});
+
+// 	});
+// });
